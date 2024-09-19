018682c9ebd2defe1d03e9ef410e6d3cbc7e33d6
TatyanaSerK <tsk.kalinina@gmail.com>
2024-09-19 09:12:11 +0300

    HW2

--- app/task1/models.py
+++ app/task1/models.py
@@ -1,36 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
-
-# 1.Buyer - модель представляющая покупателя.
-# Обладает следующими полями:
-# name - имя покупателя(username аккаунта)
-# balance - баланс(DecimalField)
-# age - возраст.
-
-class Buyer(models.Model):
-    name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=6, decimal_places=3)
-    age = models.IntegerField()
+class Buyer(models.Model):  # 1.Buyer - модель представляющая покупателя.
+    name = models.CharField(max_length=100)  # name - имя покупателя(username аккаунта)
+    balance = models.DecimalField(max_digits=10, decimal_places=3)  # balance - баланс(DecimalField)
+    age = models.IntegerField()  # age - возраст.
 
 
-# 2.Game - модель представляющая игру.
-
-# title - название игры
-# cost - цена(DecimalField)
-# size - размер файлов игры(DecimalField)
-# description - описание(неограниченное кол-во текста)
-# age_limited - ограничение возраста 18+ (BooleanField, по умолчанию False)
-# buyer - покупатель обладающий игрой (ManyToManyField). У каждого покупателя может быть игра и
-# у каждой игры может быть несколько обладателей.
-
-
-class Game(models.Model):
-    title = models.CharField(max_length = 100)
-    cost = models.DecimalField(max_digits=6, decimal_places=3)
-    size = models.DecimalField(max_digits=6, decimal_places=3)
-    models.DecimalField(max_digits=6, decimal_places=3)
-    description = models.TextField()
-    age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='game')
+class Game(models.Model):  # 2.Game - модель представляющая игру.
+    title = models.CharField(max_length=100)  # title - название игры
+    cost = models.DecimalField(max_digits=10, decimal_places=3)  # cost - цена(DecimalField)
+    size = models.DecimalField(max_digits=10, decimal_places=3)  # size - размер файлов игры(DecimalField)
+    description = models.TextField()  # description - описание(неограниченное кол-во текста)
+    age_limited = models.BooleanField(default=False)  # age_limited - ограничение возраста 18+
+    buyer = models.ManyToManyField(Buyer, related_name='game')  # buyer - покупатель обладающий игрой
